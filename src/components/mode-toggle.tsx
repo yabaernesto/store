b7c3880c0f123f1e3b96e5623f80346df298e38f
Yaba Ernesto <yabaernesto@gmail.com>
2025-04-24 04:09:43 +0100

    chore: button toggle

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -23,10 +23,10 @@ export function ThemeToggle() {
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
+          Claro
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
+          Escuro
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
           Dispositivo
